b2503aacdc18071e91e4abbfe635a1fdd2cc10c8
Mohamed <ma811forever@gmail.com>
2023-01-22 14:19:29 +0100

    Installing axios & test the endpoint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import SetUp from './components/SetUp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo}  alt="logo" />
-        <p>Setup project</p>
+        <SetUp />
       </header>
     </div>
   );
--- src/components/SetUp.tsx
+++ src/components/SetUp.tsx
@@ -1,4 +1,15 @@
-const SetUp = () => {
+import { useEffect } from "react"
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+
+const SetUp = () => {  
+  const city='London'
+  useEffect(()=> {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then(({data}) => console.log('res', data)
+    )
+  }, [])
   return( <div> 
       <p>Setup project fun</p>
     </div>)
