72ab0d7b7c8ae20e14c7fa3954de3c84914f84c7
Mohamed <ma811forever@gmail.com>
2023-01-22 14:39:49 +0100

    adding city placeholder & submit btn.

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -14,16 +14,18 @@ interface ErrorState {
 }
 
 const WeatherInfo = () => {
-
+const [city, setCity] = useState('')
   const initialWeatherState: WeatherState = {
     temperature: undefined,
     humidity: undefined,
     icon: '',
   };
   const [weather, setWeather] = useState<WeatherState>(initialWeatherState);  
-  const city='London'
-  useEffect(()=> {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+
+  const getWeather = () => {
+    console.log('get', city);
+    
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
     .then(({data}) => {
       console.log('res', data)
       setWeather({
@@ -33,12 +35,17 @@ const WeatherInfo = () => {
       })
     }
     )
-  }, [])
+  }
   console.log('weather', weather);
   
   return( 
     <div> 
       <p>Weather Info:</p>
+      <input value={city} onChange={(e)=> {
+        console.log('e', e.target.value);
+        
+        setCity(e.target.value)}} />
+      <button onClick={()=> getWeather()}>Get weather</button>
       <p>{`Temperature: ${weather?.temperature}`}</p>
       <p>{`Humidity: ${weather?.humidity}`}</p>
       <img 
